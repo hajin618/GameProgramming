4ec12f613fc14b05e622e3f0505dce6d0e83e0f7
hajin618 <1998rlagkwls@gmail.com>
2022-04-23 04:38:14 +0900

    Third Update

---
+++ 우주선게임/DrawInSDL/main_DrawInSDL.cpp
@@ -0,0 +1,62 @@
+#include "Drawing_GameFunc.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
+
+SDL_Window* g_window;
+SDL_Renderer* g_renderer;
+bool g_flag_running;
+Uint32 g_last_time_ms;
+
+int main(int argc, char* argv[]) {
+
+	// Initializing SDL library
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	// Text init
+	TTF_Init();
+
+	// 프로그램 시작할 때 Audio Open
+	// frequency : 주파수	format,	channels,	chunksize
+	// mix_dafault_frequency : 22050		44100(CD) 비교해보기
+	// 0 : success, -1 : errors
+	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+		std::cout << "Mix_OpenAudio " << Mix_GetError() << std::endl;
+		exit(1);
+	}
+
+	g_window = SDL_CreateWindow("First Window", 100, 100, 600, 750, 0);
+	g_renderer = SDL_CreateRenderer(g_window, -1, 0);
+
+	InitGame();
+
+	g_last_time_ms = SDL_GetTicks();
+
+	while (g_flag_running) {
+
+		Uint32 cur_time_ms = SDL_GetTicks();
+
+		if (cur_time_ms - g_last_time_ms < 33)
+			continue;
+
+		HandleEvents();
+		Update();
+		Render();
+
+		g_last_time_ms = cur_time_ms;
+	}
+
+	SDL_DestroyRenderer(g_renderer);
+	SDL_DestroyWindow(g_window);
+
+	ClearGame();
+
+	// 프로그램 끝날 때 Audio Close
+	Mix_CloseAudio();
+
+	// Text quit
+	TTF_Quit();
+
+	SDL_Quit();
+
+	return 0;
+}
